d08426058c0d2ad02f6fbcec876a7cfa4db17113
nnathans <nathan.wong26@ycdsbk12.ca>
2025-06-03 09:41:58 -0400

    Options
    Added options for (P)lay, (V)iew leaderboard, (A)dd theme, (Q)uit, and (S)ecret.
    
    Secret menu and Quit finished
    
    Selecting Play, View leaderboard, or add theme covers the old menu
    
    temporarily removed themes.txt from main code

--- CPTNathan.java
+++ CPTNathan.java
@@ -7,38 +7,127 @@ public class CPTNathan{
 	public static void main(String[] args){
 		Console con = new Console("Guess The Word", 1280, 720);
 		
+		// Logo
+		BufferedImage logos = con.loadImage("guesswhat.png");
+		
+		// meme images
+		BufferedImage gru = con.loadImage("gru.jpg");
+		BufferedImage troll = con.loadImage("troll.jpg");
+		
+		int intx = 1500;
+		
 		// Guess the Word
 		
 		String strmenu;
 		boolean boolquit = false;
-		
-		
-		BufferedImage logo = con.loadImage("logos.jpg");
+		boolean boolthemeselect = true;
+		boolean boolleaderboard = true;
+		boolean booladdtheme = true;
+		String strname;
 		
 		while(boolquit == false){
 			
-			con.setDrawColor(Color.WHITE);
+			// Menu ########################
+			
+			// Fill Background
+			con.setDrawColor(Color.BLACK);
 			con.fillRect(0,0,1280,720);
 			
-			con.setDrawColor(new Color(255,0,0));
+			// Options
+			con.setDrawColor(Color.WHITE);
 			con.drawString("Press one of the following keys:", 445, 100);
 			con.drawString("Press P to play", 545, 120);
 			con.drawString("Press V to view leaderboards", 465, 140);
 			con.drawString("Press A to add a theme", 495, 160);
 			con.drawString("Press Q to quit game", 505, 180);
 			
-			con.drawImage(logo, 495, 400);
+			con.drawImage(logos,495,400);
+			
+			// User Input
 		
 			strmenu = con.readLine();
+			
+			// First two loops to check if user entered a valid option
 		
 			if(strmenu.length() >= 2){
 				con.println("Only type one character.");
-			}else if(!(strmenu.equalsIgnoreCase("P") || strmenu.equalsIgnoreCase("V") || strmenu.equalsIgnoreCase("A")  || strmenu.equalsIgnoreCase("Q"))){
+			}else if(!(strmenu.equalsIgnoreCase("P") || strmenu.equalsIgnoreCase("V") || strmenu.equalsIgnoreCase("A")  || strmenu.equalsIgnoreCase("Q") || strmenu.equalsIgnoreCase("S"))){
 				con.println("That is not an option.");
 			}else if(strmenu.equalsIgnoreCase("P")){
-				TextInputFile themes = new TextInputFile("themes.txt");
+				
+				// If P is selected, it will cover the menu
+				
+				while(boolthemeselect == true){
+					con.setDrawColor(Color.BLACK);
+					con.fillRect(0,0,1280,720);
+					
+					con.repaint();
+					con.sleep(30);
+				}
+				
+				
 			
-				themes.close();
+			}else if(strmenu.equalsIgnoreCase("V")){
+				
+				// If V is selected, it will cover the menu
+				
+				while(boolleaderboard == true){
+					con.setDrawColor(Color.BLACK);
+					con.fillRect(0,0,1280,720);
+					
+					con.repaint();
+					con.sleep(30);
+				}
+				
+				
+			
+			}else if(strmenu.equalsIgnoreCase("A")){
+				
+				// If A is selected, it will cover the menu
+				
+				while(booladdtheme == true){
+					con.setDrawColor(Color.BLACK);
+					con.fillRect(0,0,1280,720);
+					
+					con.repaint();
+					con.sleep(30);
+				}
+				
+				
+			
+			}else if(strmenu.equalsIgnoreCase("S")){
+				
+				// If S is selected, it will put image of gru on screen with joke
+				
+				intx = 1500;
+				
+				while(intx > 0){
+					con.setDrawColor(Color.BLACK);
+					con.fillRect(0,0,1280,720);
+					
+					con.drawImage(gru,0,0);
+					con.drawImage(gru,730,370);
+					con.drawImage(gru,350,185);
+					
+					con.drawImage(troll,intx,300);
+					
+					intx = intx - 3;
+					
+					con.setDrawColor(Color.WHITE);
+					con.drawString("banana", 1000, 100);
+					con.drawString("banana", 1004, 109);
+					con.drawString("banana", 1013, 111);
+					
+					con.repaint();
+					con.sleep(30);
+				}
+				
+			}else if(strmenu.equalsIgnoreCase("Q")){
+				
+				// If Q is selected, it will close the console
+				
+				System.exit(0);
+				
 			}
 			
 			con.repaint();
